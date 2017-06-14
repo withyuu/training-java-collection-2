e2741b197537aaa28c32de40d4736d176eeadba9
Punaek Chayuti <punaek.cha@ascendcorp.com>
2017-06-14 11:29:22 +0700

    Placeholders

--- src/main/java/net/digitaliccat/training/di/Container.java
+++ src/main/java/net/digitaliccat/training/di/Container.java
@@ -1,50 +1,24 @@
 package net.digitaliccat.training.di;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
-
-
 public class Container {
 
-    private Map<String, Object> container;
-
     public Container() {
-        container = new HashMap<>();
+        // TODO implementation goes here
     }
 
     public Container add(String qualifier, Object obj) {
-        container.put(qualifier, (Supplier<Object>) () -> {
-            try {
-                // Limitation: only compatible with no-arg constructor
-                return obj.getClass().newInstance();
-            } catch (InstantiationException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            return null;
-        });
+        // TODO implementation goes here
         return this;
     }
 
     public Container addAsSingleton(String qualifier, Object obj) {
-        container.put(qualifier, obj);
+        // TODO implementation goes here
         return this;
     }
 
     public Object get(String qualifier) {
-        Object obj = container.get(qualifier);
-        if (obj == null) {
-            throw new IllegalArgumentException("No object found with qualifier: " + qualifier);
-        }
-
-        if (obj instanceof Supplier) {
-            Supplier<Object> sup = (Supplier<Object>) obj;
-            return sup.get();
-        }
-
-        return obj;
+        // TODO implementation goes here
+        return null;
     }
 
 }
